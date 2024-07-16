3fffd9097d8d896e15dd77b026c661faa88758ca
Francisco <francisco@MacBook-Air-de-Francisco.local>
2024-07-15 18:50:43 -0600

    Character card

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,32 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters: [], //almacenar todos los personajes
+    charactersFilter: [] //consultas & filtros para nuestro proyecto
   },
   getters: {
   },
   mutations: {
+    setCharacters(state, payload){
+      state.characters = payload
+    },
+    setCharactersFilter(state, payload){
+      state.charactersFilter = payload
+    }
   },
   actions: {
+    async getCharacters({commit}){
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json();
+        console.log(data);
+        commit('setCharacters', data.results)
+        commit('setCharactersFilter', data.results)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
   modules: {
   }
